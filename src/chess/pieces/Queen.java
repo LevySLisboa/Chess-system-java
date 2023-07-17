82f152e41be20e405a183ac0f26a2752a73321be
Levy Lisboa <lisboalevy@gmail.com>
2023-07-17 17:18:07 -0300

    Rainha Adicionada

---
+++ src/chess/pieces/Queen.java
@@ -0,0 +1,113 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Queen extends ChessPiece {
+    public Queen(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        Position p = new Position(0,0);
+
+
+        //Cima
+        p.setValues(position.getRows()-1,position.getColumns());
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRows()][p.getColumns()] = true;
+            p.setRows(p.getRows()-1);
+        }
+
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRows()][p.getColumns()] = true;
+        }
+
+        //Baixo
+        p.setValues(position.getRows()+1,position.getColumns());
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRows()][p.getColumns()] = true;
+            p.setRows(p.getRows()+1);
+        }
+
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRows()][p.getColumns()] = true;
+        }
+
+        //Esquerda
+        p.setValues(position.getRows(),position.getColumns()-1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRows()][p.getColumns()] = true;
+            p.setColumns(p.getColumns()-1);
+        }
+
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRows()][p.getColumns()] = true;
+        }
+        //Direita
+        p.setValues(position.getRows(),position.getColumns()+1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRows()][p.getColumns()] = true;
+            p.setColumns(p.getColumns()+1);
+        }
+
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRows()][p.getColumns()] = true;
+        }
+        //nw
+        p.setValues(position.getRows()-1,position.getColumns()-1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRows()][p.getColumns()] = true;
+            p.setValues(p.getRows()-1,p.getColumns()-1);
+        }
+
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRows()][p.getColumns()] = true;
+        }
+
+        //ne
+        p.setValues(position.getRows()-1,position.getColumns()+1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRows()][p.getColumns()] = true;
+            p.setValues(p.getRows()-1,p.getColumns()+1);
+        }
+
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRows()][p.getColumns()] = true;
+        }
+
+        //se
+        p.setValues(position.getRows()+1,position.getColumns()+1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRows()][p.getColumns()] = true;
+            p.setValues(p.getRows()+1,p.getColumns()+1);
+        }
+
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRows()][p.getColumns()] = true;
+        }
+
+        //sw
+        p.setValues(position.getRows()+1,position.getColumns()-1);
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRows()][p.getColumns()] = true;
+            p.setValues(p.getRows()+1,p.getColumns()-1);
+        }
+
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRows()][p.getColumns()] = true;
+        }
+
+        return mat;
+    }
+
+    @Override
+    public String toString() {
+        return "Q";
+    }
+}
